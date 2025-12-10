d8150acc2bc6ca6e2e5b3fc699fb0177bb93bab4
Oleksandr <alex.sherbyna1@gmail.com>
2025-12-10 16:47:47 +0200

    first commit

---
+++ src/test/java/com/example/BestOFBooking/BestOfBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BestOFBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestOfBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
