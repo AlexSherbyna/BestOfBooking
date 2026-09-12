1d4b9d89d1b2394874c264a77685dc9f5dc26d74
Oleksandr <alex.sherbyna1@gmail.com>
2026-09-12 16:12:30 +0300

    removed imports

--- src/main/java/com/bob/BobApplication.java
+++ src/main/java/com/bob/BobApplication.java
@@ -2,16 +2,12 @@ package com.bob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-
 public class BobApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BobApplication.class, args);
 	}
-
 
 }
